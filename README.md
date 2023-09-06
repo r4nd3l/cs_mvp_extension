fb84956986fc12cf065205661407802e00590157
Molnar Mate <molnar.mate@evista.hu>
2023-09-06 15:35:24 +0200

    firs commit and wip manifest v3

--- README.md
+++
@@ -1,2 +0,0 @@
-# cs_mvp_extension
-Test extension for cs_mvp
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+let mix = require("laravel-mix");
+
+mix
+  .setPublicPath("./")
+  .sass("assets/sass/popup.scss", "dist/css")
+  .js("assets/js/background.js", "dist/js")
+  .options({
+    processCssUrls: false,
+  });
